196d1ce3e059011a0c0b26bee2f8fe7ae0a00207
Crowdbotics <team@crowdbotics.com>
2023-09-11 17:41:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-brook-43334",
+  url: "https://weathered-brook-43334.botics.co",
+  api: "https://weathered-brook-43334.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
